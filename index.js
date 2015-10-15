db81d43350ecda9da478ba566a9b9871d4b2e46d
Jason Pincin <jason@pincin.com>
2015-10-14 20:47:41 -0400

    eventuate-core is too primitive for end/error

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
 var basicEventuate           = require('./basic'),
-    EventuateUnconsumedError = require('./errors').EventuateUnconsumedError,
-    EventuateEndedError      = require('./errors').EventuateEndedError
+    EventuateUnconsumedError = require('./errors').EventuateUnconsumedError
 
 module.exports = function createEventuate (options) {
     options = typeof options === 'object' ? options : {}
     options.requireConsumption = options.requireConsumption === undefined ? false : options.requireConsumption
 
-    var ended = false
-
     var eventuate = Object.defineProperties(basicEventuate(), {
         produce        : { value: produce, configurable: true, writable: true },
         consume        : { value: consume, configurable: true, writable: true },
         removeConsumer : { value: removeConsumer, configurable: true, writable: true },
-        signalError    : { value: signalError, configurable: true, writable: true },
-        signalEnd      : { value: signalEnd, configurable: true, writable: true },
         consumerAdded  : { value: basicEventuate(), configurable: true, writable: true },
         consumerRemoved: { value: basicEventuate(), configurable: true, writable: true },
-        error          : { value: basicEventuate(), configurable: true, writable: true },
-        end            : { value: basicEventuate(), configurable: true, writable: true },
         factory        : { value: createEventuate, configurable: true, writable: true }
     })
 
     return eventuate
 
     function produce (data) {
-        if (ended)
-            throw new EventuateEndedError('Cannot produce data on an ended eventuate')
         if (options.requireConsumption && eventuate._consumers.length === 0)
             throw ((data instanceof Error) ? data : new EventuateUnconsumedError('Unconsumed eventuate data', { data: data }))
 
@@ -43,14 +34,5 @@ module.exports = function createEventuate (options) {
     function removeConsumer (consumer) {
         eventuate._consumers.splice(eventuate._consumers.indexOf(consumer), 1)
         eventuate.consumerRemoved.produce(consumer)
-    }
-
-    function signalError (err) {
-        eventuate.error.produce(err)
-    }
-
-    function signalEnd () {
-        ended = true
-        eventuate.end.produce()
     }
 }
