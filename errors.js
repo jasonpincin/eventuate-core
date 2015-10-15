8098d2f6129f2539911ea53f2426c23a2ace7671
Jason Pincin <jason@pincin.com>
2015-10-14 20:29:31 -0400

    Removed un-used error.

--- errors.js
+++ errors.js
@@ -2,7 +2,6 @@ var defineError = require('define-error'),
     assign      = require('object-assign')
 
 module.exports.EventuateUnconsumedError = defineError('EventuateUnconsumedError', assignData)
-module.exports.EventuateEndedError = defineError('EventuateEndedError')
 
 function assignData (_, data) {
     assign(this, data)
